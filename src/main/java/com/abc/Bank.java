033e7aa4a2e634bf1ec00bb846e5276f598ee951
Mandalorian007 <mandalorian007@gmail.com>
2017-03-28 23:39:20 -0400

    implemented daily interest accruement

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -1,13 +1,21 @@
 package com.abc;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Bank {
     private final List<Customer> customers;
 
     public Bank() {
         customers = new ArrayList<>();
+        InterestAccruementTask interestAccruementTask = new InterestAccruementTask(this);
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(interestAccruementTask, getHoursUntilTarget(1), 24, TimeUnit.HOURS);
     }
 
     public void addCustomer(Customer customer) {
@@ -21,6 +29,10 @@ public class Bank {
         return summary;
     }
 
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
     //Make sure correct plural of word is created based on the number passed in:
     //If number passed in is 1 just return the word otherwise add an 's' at the end
     private String format(int number, String word) {
@@ -31,5 +43,11 @@ public class Bank {
         return customers.stream()
                 .mapToDouble(Customer::totalInterestEarned)
                 .sum();
+    }
+
+    private int getHoursUntilTarget(int targetHour) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        return hour < targetHour ? targetHour - hour : targetHour - hour + 24;
     }
 }
